156dfe936419f7a22d9eadacc71a31daa340a5ef
Ashwinidborkar <ashwini.borkar@cloudreach.com>
2023-05-22 19:24:12 +0530

    version added

--- provider.tf
+++ provider.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# terraform {
-#   required_version = ">= 1.1.0"
-# }
+terraform {
+  required_version = ">= 1.1.0"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.66.1 "
+    }
+  }
+}
