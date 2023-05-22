91a0092dc469126d7110aabcbfc55b57c417bc8e
Ashwinidborkar <ashwini.borkar@cloudreach.com>
2023-05-22 12:54:16 +0530

    role created

--- autoscaling.tf
+++ autoscaling.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "mglab_autogrp" {
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 2
-  force_delete              = true
+  #force_delete              = true
   #placement_group           = aws_placement_group.test.id
   launch_configuration      = aws_launch_configuration.asg_launch_conf.name
   vpc_zone_identifier       = [data.aws_subnet.public_1.id,data.aws_subnet.public_2.id]
