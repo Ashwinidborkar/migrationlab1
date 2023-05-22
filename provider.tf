1a5e1c6e7b04b60c8eeb1988849fa734b326bf0d
Ashwinidborkar <ashwini.borkar@cloudreach.com>
2023-05-22 19:10:18 +0530

    fmt done 1

--- provider.tf
+++ provider.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-terraform {
-  required_version = ">= 1.1.0"
-}
-
-
+# terraform {
+#   required_version = ">= 1.1.0"
+# }
--- vpc.tf
+++ vpc.tf
@@ -12,8 +12,6 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.1"
 
-
-
   name = local.name
   cidr = local.vpc_cidr
 
