961f94612a5eefc737ea757a21b1d2b75036b82f
Ashwinidborkar <ashwini.borkar@cloudreach.com>
2023-05-22 14:57:20 +0530

    changes reverted

--- locals.tf
+++ locals.tf
@@ -16,7 +16,7 @@
 
 
 locals {
-  availability_zones = slice(data.aws_availability_zones.available.names, 1, var.number_of_azs)
+  availability_zones = slice(data.aws_availability_zones.available.names, 0, var.number_of_azs)
 
   public_subnet_cidr   = cidrsubnet(var.vpc_cidr, 1, 0)
   private_subnet_cidr  = cidrsubnet(var.vpc_cidr, 1, 1)
