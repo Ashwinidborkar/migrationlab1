f091fa471c1f88f437bb5bacd202ee230519d3c2
Ashwinidborkar <ashwini.borkar@cloudreach.com>
2023-05-23 17:01:16 +0530

    sample vpc added for infracost

--- ec.tf
+++ ec.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "my_sample_ec2" {
-  ami           = "ami-03aefa83246f44ef2"
-  instance_type = "t2.micro"
+resource "aws_vpc" "samplevpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 }
